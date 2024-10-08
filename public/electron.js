f37216b2f643b3e0f487558a8f0beab8188def99
Elliot <admin@elliot-at-helsinki.eu>
2024-10-08 23:18:44 +0300

    Electron for Linux

--- public/electron.js
+++ public/electron.js
@@ -1,39 +1,40 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
 
-let mainWindow;
+let mainWindow
 
-function createWindow() {
+const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'media/icons/electron.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
-  });
+  })
 
   mainWindow.loadURL(
     isDev
       ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  );
+      : `file://${path.join(__dirname, '..', 'build', 'index.html')}`
+  )
 
-  mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.on('closed', () => (mainWindow = null))
 }
 
-app.disableHardwareAcceleration();
-app.on('ready', createWindow);
+app.disableHardwareAcceleration()
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
